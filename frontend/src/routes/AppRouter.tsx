15f07178a249ada9fa2f58ed34e67389ba7e6878
Jivesh21 <jiveshsharma2220@gmail.com>
2026-08-30 14:59:18 +0530

    your message

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import ProjectsPage from "@/pages/ProjectsPage"
 import DashboardPage from "@/pages/DashboardPage"
 import MyWorkPage from "@/pages/MyWorkPage"
-
+import TeamsPage from "@/pages/TeamsPage"
 function AppRouter() {
   return (
     <BrowserRouter>
@@ -16,6 +16,10 @@ function AppRouter() {
           path="/dashboard"
           element={<DashboardPage />}
         />
+        <Route
+  path="/teams"
+  element={<TeamsPage />}
+/>
 <Route
   path="/projects"
   element={<ProjectsPage />}
